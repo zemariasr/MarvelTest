34ef6e8c5074861d0211208764d78d1706363417
José Maria <zemariasr@me.com>
2020-05-12 13:01:39 -0300

    App feito para um teste de aptidão
    Este app usa recursos do API da Marvel. Usarei alguns Frameworks. Farei de uma forma simples e agradável buscando conforto e praticidade para o usuário.

---
+++ Teste_Marvel/Teste_Marvel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teste_Marvel
+//
+//  Created by José Maria Só Rodrigues on 12/05/20.
+//  Copyright © 2020 ZM System. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Teste_Marvel/Teste_MarvelTests/Teste_MarvelTests.swift
@@ -0,0 +1,34 @@
+//
+//  Teste_MarvelTests.swift
+//  Teste_MarvelTests
+//
+//  Created by José Maria Só Rodrigues on 12/05/20.
+//  Copyright © 2020 ZM System. All rights reserved.
+//
+
+import XCTest
+@testable import Teste_Marvel
+
+class Teste_MarvelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
